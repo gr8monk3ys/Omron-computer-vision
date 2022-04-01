d82813775d32fbb95c543292f9b17ad6d7ae0e87
danibitar <92876612+danibitar@users.noreply.github.com>
2022-04-01 12:27:04 -0700

    Add files via upload

---
+++ apiTest.py
@@ -0,0 +1,13 @@
+
+class Interface:
+    def load_pic(self):  # input: signal to get pic from back
+        # output: send pic to front for display
+        pass
+
+    def get_data(self):  # get signal from front
+        # send signal to output csv
+        pass
+
+    def classify_pic(self):  # get signal from front
+        # send signal to classify pic
+        pass
